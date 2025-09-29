96ff2a9966df694091d72b87c6eb4ab86b649801
taemin6536 <xoals6536@naver.com>
2025-09-29 09:24:34 +0900

    feat: update pdf AI

---
+++ chap04/pdf_to_txt.py
@@ -0,0 +1,20 @@
+import pymupdf
+import os
+
+pdf_file_path = 'data/과정기반 작물모형을 이용한 웹 기반 밀 재배관리 의사결정 지원시스템 설계 및 구축.pdf'
+doc = pymupdf.open(pdf_file_path)
+
+full_text = ""
+
+for page in doc:
+    text = page.get_text()
+    full_text += text
+
+pdf_file_name = os.path.basename(pdf_file_path)
+pdf_file_name = os.path.splitext(pdf_file_name)[0]
+
+txt_file_path = f"chap04/output/{pdf_file_name}.txt"
+with open(txt_file_path, 'w', encoding='utf-8') as f:
+    f.write(full_text)
+
+print(f"PDF to TXT conversion complete: {txt_file_path}")
